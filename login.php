425209146ce156b227c295630bd6c65ef5932baf
Kingsley <kingsleyumujeyan@gmail.com>
2019-11-07 12:54:09 +0100

    Navbar Updated

--- login.php
+++ login.php
@@ -193,13 +193,13 @@ if(isset($_POST['submit'])){
 
     <header>
           <nav class="navbar navbar-expand-lg navbar-light scrolling-navbar fixed-top">
-            <a class="navbar-brand px-sm-5 ml-3" href="index.html"><img src="https://res.cloudinary.com/kuic/image/upload/v1572638901/docufix/Docufix_Logo_lnsgsr.svg" alt="DOCUFIX" id="image"></a>
+            <a class="navbar-brand" href="index.html"><img src="https://res.cloudinary.com/kuic/image/upload/v1572638901/docufix/Docufix_Logo_lnsgsr.svg" alt="DOCUFIX" id="image"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
           
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ml-auto px-5">
+              <ul class="navbar-nav ml-auto mt-2">
                 <li class="nav-item">
                   <a class="nav-link text-center" href="signup.php">Sign Up</a>
                 </li>
